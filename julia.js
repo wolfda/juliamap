b95ca741f04404e94a69a576bbb8f044232550a2
Daniel Wolf <daniel@Daniels-MacBook-Pro.local>
2025-02-22 11:59:55 -0800

    Orbit refactoring

--- julia.js
+++ julia.js
@@ -1,3 +1,5 @@
+import { screenToComplex } from "./map.js";
+
 export function getEscapeVelocity(x0, y0, maxIter) {
     let x = 0;
     let y = 0;
@@ -37,4 +39,59 @@ export function getJuliaSeries(x0, y0, count) {
         y = y2;
     }
     return points;
+}
+
+
+/**
+ * An orbit is a reference point in the comlpex plan, with the precomputed Julia series.
+ */
+export class Orbit {
+    /**
+     * Search for the orbit with the heighest escape velocity in the current viewport.
+     */
+    static searchMaxEscapeVelocity(width, height, maxIter, maxSamples = 200) {
+        let bestOrbit = null;
+
+        for (let s = 0; s < maxSamples; s++) {
+            const sx = Math.random() * width;
+            const sy = Math.random() * height;
+            const orbit = new Orbit(sx, sy).withEscapeVelocity(width, height, maxIter);
+            if (bestOrbit === null || orbit.escapeVelocity > bestOrbit.escapeVelocity) {
+                bestOrbit = orbit;
+            }
+            if (bestOrbit.escapeVelocity === maxIter) {
+                break;
+            }
+        }
+        return bestOrbit.withJuliaSeries(width, height, maxIter);
+    }
+
+    /**
+     * @param {number} sx screen x coordinate, in [0, width] 
+     * @param {number} sy screen y coordinate, in [0, height]
+     */
+    constructor(sx, sy) {
+        this.sx = sx;
+        this.sy = sy;
+        this.escapeVelocity = null;
+        this.iters = null;
+    }
+    
+    /**
+     * Compute the escape velocity for the current orbit.
+     */
+    withEscapeVelocity(width, height, maxIter) {
+        const candidate = screenToComplex(this.sx, this.sy, width, height);
+        this.escapeVelocity = getEscapeVelocity(candidate.cx, candidate.cy, maxIter);
+        return this;
+    }
+
+    /**
+     * Compute the Julia series for the current coordinate.
+     */
+    withJuliaSeries(width, height, maxIter) {
+        const candidate = screenToComplex(this.sx, this.sy, width, height);
+        this.iters = getJuliaSeries(candidate.cx, candidate.cy, maxIter);    
+        return this;
+    }
 }
